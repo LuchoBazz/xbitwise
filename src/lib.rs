751f947b0f96325577fadbeab8f57852bff5da20
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2022-12-10 20:49:13 -0500

    feat: add parity, hamming_distance and bit_size methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,41 @@
 pub trait Bitwise: private::Sealed {}
 
 mod private {
-    pub trait Sealed: Copy {
-        type Type;
+    use std::ops::{Add, BitAnd, BitOr, BitXor, Div, Mul, Not, Shl, Shr, Sub};
 
-        fn set_bit_unchecked(self, bit: usize, value: bool) -> Self;
-        fn set_bit(self, bit: usize, value: bool) -> Option<Self::Type>;
-        fn set_high_unchecked(self, bit: usize) -> Self;
-        fn set_high(self, bit: usize) -> Option<Self::Type>;
-        fn set_low_unchecked(self, bit: usize) -> Self;
-        fn set_low(self, bit: usize) -> Option<Self::Type>;
+    pub trait Sealed:
+        Sized
+        + Copy
+        + Not<Output = Self>
+        + BitOr<Output = Self>
+        + BitXor<Output = Self>
+        + Add<Output = Self>
+        + Sub<Output = Self>
+        + Mul<Output = Self>
+        + Div<Output = Self>
+        + Shr<u32, Output = Self>
+        + Shl<u32, Output = Self>
+        + BitAnd<Output = Self>
+        + Eq
+        + PartialOrd
+    {
+        fn zero() -> Self;
+        fn one() -> Self;
+
+        fn set_bit_to_unchecked(self, bit: usize, new_value: bool) -> Self;
+        fn set_bit_to(self, bit: usize, new_value: bool) -> Option<Self>;
+        fn set_to_high_unchecked(self, bit: usize) -> Self;
+        fn set_to_high(self, bit: usize) -> Option<Self>;
+        fn set_to_low_unchecked(self, bit: usize) -> Self;
+        fn set_to_low(self, bit: usize) -> Option<Self>;
         fn flip_bit_unchecked(self, bit: usize) -> Self;
-        fn flip_bit(self, bit: usize) -> Option<Self::Type>;
+        fn flip_bit(self, bit: usize) -> Option<Self>;
         fn flip_all_unchecked(self) -> Self;
-        fn flip_all(self) -> Option<Self::Type>;
+        fn flip_all(self) -> Option<Self>;
+
+        fn parity(self) -> usize;
+        fn hamming_distance(self, other: Self) -> usize;
+        fn bit_size() -> usize;
 
         fn has_high_bit_unchecked(self, bit: usize) -> bool;
         fn has_high_bit(self, bit: usize) -> Option<bool>;
@@ -36,44 +58,49 @@ macro_rules! impl_bitwise {
 
         impl private::Sealed for $t {
 
-            type Type = $t;
+            fn zero() -> Self {
+                0
+            }
+            fn one() -> Self {
+                1
+            }
 
-            fn set_bit_unchecked(self, bit: usize, value: bool) -> Self {
-                if value {
-                    self.set_high_unchecked(bit)
+            fn set_bit_to_unchecked(self, bit: usize, new_value: bool) -> Self {
+                if new_value {
+                    self.set_to_high_unchecked(bit)
                 } else {
-                    self.set_low_unchecked(bit)
+                    self.set_to_low_unchecked(bit)
                 }
             }
 
-            fn set_bit(self, bit: usize, value: bool) -> Option<Self::Type> {
+            fn set_bit_to(self, bit: usize, new_value: bool) -> Option<Self> {
                 check_bit_index_or_return_none!(bit, $max_bits);
-                Some(self.set_bit_unchecked(bit, value))
+                Some(self.set_bit_to_unchecked(bit, new_value))
             }
 
-            fn set_high_unchecked(self, bit: usize) -> Self {
-                self | (1 << bit)
+            fn set_to_high_unchecked(self, bit: usize) -> Self {
+                self | (Self::one() << bit)
             }
 
-            fn set_high(self, bit: usize) -> Option<Self::Type> {
+            fn set_to_high(self, bit: usize) -> Option<Self> {
                 check_bit_index_or_return_none!(bit, $max_bits);
-                Some(self.set_high_unchecked(bit))
+                Some(self.set_to_high_unchecked(bit))
             }
 
-            fn set_low_unchecked(self, bit: usize) -> Self {
-                self & !((1 as Self) << (bit as Self))
+            fn set_to_low_unchecked(self, bit: usize) -> Self {
+                self & !(Self::one() << bit)
             }
 
-            fn set_low(self, bit: usize) -> Option<Self::Type> {
+            fn set_to_low(self, bit: usize) -> Option<Self> {
                 check_bit_index_or_return_none!(bit, $max_bits);
-                Some(self.set_low_unchecked(bit))
+                Some(self.set_to_low_unchecked(bit))
             }
 
             fn flip_bit_unchecked(self, bit: usize) -> Self {
-                self ^ (1 << bit)
+                self ^ (Self::one() << bit)
             }
 
-            fn flip_bit(self, bit: usize) -> Option<Self::Type> {
+            fn flip_bit(self, bit: usize) -> Option<Self> {
                 check_bit_index_or_return_none!(bit, $max_bits);
                 Some(self.flip_bit_unchecked(bit))
             }
@@ -82,12 +109,24 @@ macro_rules! impl_bitwise {
                 !self
             }
 
-            fn flip_all(self) -> Option<Self::Type> {
+            fn flip_all(self) -> Option<Self> {
                 Some(self.flip_all_unchecked())
             }
 
+            fn parity(self) -> usize {
+                (self.count_ones() as usize) & (Self::one() as usize)
+            }
+
+            fn hamming_distance(self, other: Self) -> usize {
+                (self ^ other).count_ones() as usize
+            }
+
+            fn bit_size() -> usize {
+                $max_bits
+            }
+
             fn has_high_bit_unchecked(self, bit: usize) -> bool {
-                let mask = (1 << bit);
+                let mask = (Self::one() << bit);
                 self & mask == mask
             }
 
