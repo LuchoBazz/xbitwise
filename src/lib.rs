d29ef419b0bb2a8f8f8466b9d06315fdc32d32cb
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2022-12-14 13:20:46 -0500

    test: added test about all methods

--- src/lib.rs
+++ src/lib.rs
@@ -20,29 +20,7 @@ pub trait Bitwise:
 {
     /// ```rust
     /// use xbitwise::Bitwise;
-    /// 
-    /// assert_eq!(i8::zero(), 0i8);
-    /// ```
-    fn zero() -> Self;
-
-    /// ```rust
-    /// use xbitwise::Bitwise;
-    /// 
-    /// assert_eq!(i8::one(), 1i8);
-    /// ```
-    fn one() -> Self;
-
-    /// ```rust
-    /// use xbitwise::Bitwise;
-    /// 
-    /// let expected: usize = 8;
-    /// assert_eq!(i8::bit_size(), 8usize);
-    /// ```
-    fn bit_size() -> usize;
-
-    /// ```rust
-    /// use xbitwise::Bitwise;
-    /// 
+    ///
     /// let number: i8 = 0b00010;
     /// let other = number.get_bit_unchecked(1);
     /// assert_eq!(other, true);
@@ -51,37 +29,169 @@ pub trait Bitwise:
 
     /// ```rust
     /// use xbitwise::Bitwise;
-    /// 
+    ///
     /// let number: i8 = 0b00010;
     /// let other = number.get_bit(1);
     /// assert_eq!(other, Some(true));
     /// ```
     fn get_bit(self, index: usize) -> Option<bool>;
+
+    /// ```rust
+    /// use xbitwise::Bitwise;
+    ///
+    /// let number: i8 = 0b00010;
+    /// let other = number.set_bit_unchecked(2);
+    /// assert_eq!(other, 0b00110);
+    /// ```
     fn set_bit_unchecked(self, index: usize) -> Self;
+
+    /// ```rust
+    /// use xbitwise::Bitwise;
+    ///
+    /// let number: i8 = 0b00010;
+    /// let other: Option<i8> = number.set_bit(2);
+    /// assert_eq!(other, Some(0b00110));
+    /// ```
     fn set_bit(self, index: usize) -> Option<Self>;
+
+    /// ```rust
+    /// use xbitwise::Bitwise;
+    ///
+    /// let number: i32 = 0b100;
+    ///
+    /// let other= number.set_range(..10);
+    /// assert_eq!(other, 0b1111111111);
+    ///
+    /// let other= number.set_range(0..10);
+    /// assert_eq!(other, 0b1111111111);
+    ///
+    /// let other= number.set_range(5..7);
+    /// assert_eq!(other, 0b01100100);
+    ///
+    /// let other= number.set_range(5..=7);
+    /// assert_eq!(other, 0b11100100);
+    /// ```
     fn set_range<R: RangeBounds<Self>>(self, range: R) -> Self;
-    fn set(self) -> Self;
+
+    /// ```rust
+    /// use xbitwise::Bitwise;
+    ///
+    /// let other: u8 = 0b100.set_all();
+    /// assert_eq!(other, 0b11111111);
+    ///
+    /// let other: u16 = 0b100.set_all();
+    /// assert_eq!(other, 0b1111111111111111);
+    ///
+    /// let other: u32 = 0b100.set_all();
+    /// assert_eq!(other, 0b11111111111111111111111111111111);
+    /// ```
+    fn set_all(self) -> Self;
+
+    /// ```rust
+    /// use xbitwise::Bitwise;
+    ///
+    /// let other: i8 = 0b00010.update_bit_unchecked(4, true);
+    /// assert_eq!(other, 0b10010);
+    /// ```
     fn update_bit_unchecked(self, index: usize, new_value: bool) -> Self;
 
     /// ```rust
     /// use xbitwise::Bitwise;
-    /// 
-    /// let number: i8 = 0b00010;
-    /// let expected: i8 = 0b10010;
-    /// let other = number.update_bit(4, true).unwrap();
-    /// assert_eq!(other, expected);
+    ///
+    /// let other: Option<i8> = 0b00010.update_bit(4, true);
+    /// assert_eq!(other, Some(0b10010));
     /// ```
     fn update_bit(self, index: usize, new_value: bool) -> Option<Self>;
+
+    /// ```rust
+    /// use xbitwise::Bitwise;
+    ///
+    /// let other: i8 = 0b00010001.clear_bit_unchecked(4);
+    /// assert_eq!(other, 0b00000001);
+    /// ```
     fn clear_bit_unchecked(self, index: usize) -> Self;
+
+    /// ```rust
+    /// use xbitwise::Bitwise;
+    ///
+    /// let other: Option<i8> = 0b00010001.clear_bit(4);
+    /// assert_eq!(other, Some(0b00000001));
+    /// ```
     fn clear_bit(self, index: usize) -> Option<Self>;
+
+    /// ```rust
+    /// use xbitwise::Bitwise;
+    ///
+    /// let other: i8 = 0b00010001.clear();
+    /// assert_eq!(other, 0b00000000);
+    /// ```
     fn clear(self) -> Self;
+
+    /// ```rust
+    /// use xbitwise::Bitwise;
+    ///
+    /// let other: i8 = 0b00010001.flip_bit_unchecked(2);
+    /// assert_eq!(other, 0b00010101);
+    /// ```
     fn flip_bit_unchecked(self, index: usize) -> Self;
+
+    /// ```rust
+    /// use xbitwise::Bitwise;
+    ///
+    /// let other: Option<i8> = 0b00010001.flip_bit(2);
+    /// assert_eq!(other, Some(0b00010101));
+    /// ```
     fn flip_bit(self, index: usize) -> Option<Self>;
+
+    /// ```rust
+    /// use xbitwise::Bitwise;
+    ///
+    /// #[allow(overflowing_literals)]
+    /// let other: i8 = 0b10010001.flip();
+    /// assert_eq!(other, 0b01101110);
+    /// ```
     fn flip(self) -> Self;
 
-    fn parity(self) -> usize;
+    /// ```rust
+    /// use xbitwise::Bitwise;
+    ///
+    /// let other: bool = 0b00000001.parity();
+    /// assert_eq!(other, true);
+    ///
+    /// let other: bool = 0b10000001.parity();
+    /// assert_eq!(other, false);
+    /// ```
+    fn parity(self) -> bool;
 
+    /// ```rust
+    /// use xbitwise::Bitwise;
+    ///
+    /// let other: usize = 0b00000000.hamming_distance(0b11000001);
+    /// assert_eq!(other, 3);
+    /// ```
     fn hamming_distance(self, other: Self) -> usize;
+
+    /// ```rust
+    /// use xbitwise::Bitwise;
+    ///
+    /// assert_eq!(i8::zero(), 0i8);
+    /// ```
+    fn zero() -> Self;
+
+    /// ```rust
+    /// use xbitwise::Bitwise;
+    ///
+    /// assert_eq!(i8::one(), 1i8);
+    /// ```
+    fn one() -> Self;
+
+    /// ```rust
+    /// use xbitwise::Bitwise;
+    ///
+    /// let expected: usize = 8;
+    /// assert_eq!(i8::bit_size(), 8usize);
+    /// ```
+    fn bit_size() -> usize;
 }
 
 macro_rules! check_bit_index_or_return_none {
@@ -97,25 +207,14 @@ macro_rules! impl_bitwise {
 
         impl Bitwise for $t {
 
-            fn zero() -> Self {
-                0
+            fn get_bit_unchecked(self, index: usize) -> bool {
+                let mask = (Self::one() << index);
+                (self & mask) == mask
             }
 
-            fn one() -> Self {
-                1
-            }
-
-            fn update_bit_unchecked(self, index: usize, new_value: bool) -> Self {
-                if new_value {
-                    self.set_bit_unchecked(index)
-                } else {
-                    self.clear_bit_unchecked(index)
-                }
-            }
-
-            fn update_bit(self, index: usize, new_value: bool) -> Option<Self> {
+            fn get_bit(self, index: usize) -> Option<bool> {
                 check_bit_index_or_return_none!(index, $max_bits);
-                Some(self.update_bit_unchecked(index, new_value))
+                Some(self.get_bit_unchecked(index))
             }
 
             fn set_bit_unchecked(self, index: usize) -> Self {
@@ -127,6 +226,36 @@ macro_rules! impl_bitwise {
                 Some(self.set_bit_unchecked(index))
             }
 
+            fn set_range<R: RangeBounds<Self>>(self, range: R) -> Self {
+                let left = match range.start_bound() {
+                    Included(val) => (*val) as Self,
+                    Excluded(val) => (*val) + 1 as Self,
+                    _ => 0,
+                };
+
+                let right = match range.end_bound() {
+                    Included(val) => (*val) as Self,
+                    Excluded(val) => (*val) - 1 as Self,
+                    _ => ($max_bits as Self) - 2,
+                };
+
+                let range = (((1 << left) - 1) ^ ((1 << right) - 1)) | (1 << right);
+                self | range
+            }
+
+            fn set_all(self) -> Self {
+                self.clear().flip()
+            }
+
+            fn update_bit_unchecked(self, index: usize, new_value: bool) -> Self {
+                if new_value { self.set_bit_unchecked(index) } else { self.clear_bit_unchecked(index) }
+            }
+
+            fn update_bit(self, index: usize, new_value: bool) -> Option<Self> {
+                check_bit_index_or_return_none!(index, $max_bits);
+                Some(self.update_bit_unchecked(index, new_value))
+            }
+
             fn clear_bit_unchecked(self, index: usize) -> Self {
                 self & !(Self::one() << index)
             }
@@ -134,6 +263,10 @@ macro_rules! impl_bitwise {
             fn clear_bit(self, index: usize) -> Option<Self> {
                 check_bit_index_or_return_none!(index, $max_bits);
                 Some(self.clear_bit_unchecked(index))
+            }
+
+            fn clear(self) -> Self {
+                self & 0
             }
 
             fn flip_bit_unchecked(self, index: usize) -> Self {
@@ -149,49 +282,19 @@ macro_rules! impl_bitwise {
                 !self
             }
 
-            fn clear(self) -> Self {
-                self & 0
-            }
-
-            fn set_range<R: RangeBounds<Self>>(self, range: R) -> Self {
-                let left = match range.start_bound() {
-                    Included(val) => (*val) as Self,
-                    _ => 0,
-                };
-                let right = match range.end_bound() {
-                    Included(val) => (*val) as Self,
-                    _ => ($max_bits as Self) - 1,
-                };
-
-                let range = (((1 << left) - 1) ^ ((1 << right) - 1)) | (1 << right);
-                self | range
-            }
-
-            fn set(self) -> Self {
-                self.clear().flip()
-            }
-
-            fn parity(self) -> usize {
-                (self.count_ones() as usize) & (Self::one() as usize)
+            fn parity(self) -> bool {
+                ((self.count_ones() as Self) & Self::one()) == Self::one()
             }
 
             fn hamming_distance(self, other: Self) -> usize {
                 (self ^ other).count_ones() as usize
             }
 
-            fn bit_size() -> usize {
-                $max_bits
-            }
+            fn zero() -> Self { 0 }
 
-            fn get_bit_unchecked(self, index: usize) -> bool {
-                let mask = (Self::one() << index);
-                (self & mask) == mask
-            }
+            fn one() -> Self { 1 }
 
-            fn get_bit(self, index: usize) -> Option<bool> {
-                check_bit_index_or_return_none!(index, $max_bits);
-                Some(self.get_bit_unchecked(index))
-            }
+            fn bit_size() -> usize { $max_bits }
         }
     )*};
 }
@@ -243,21 +346,6 @@ mod tests {
     use crate::Bitwise;
 
     #[test]
-    fn zero() {
-        assert_eq!(i8::zero(), 0i8);
-    }
-
-    #[test]
-    fn one() {
-        assert_eq!(i8::one(), 1i8);
-    }
-    
-    #[test]
-    fn bit_size() {
-        assert_eq!(i8::bit_size(), 8usize);
-    }
-
-    #[test]
     fn get_bit_unchecked() {
         let number: i8 = 0b00010;
         let other = number.get_bit_unchecked(1);
@@ -272,10 +360,133 @@ mod tests {
     }
 
     #[test]
-    fn update_bit() {
+    fn set_bit_unchecked() {
         let number: i8 = 0b00010;
-        let expected: i8 = 0b10010;
-        let other = number.update_bit(4, true).unwrap();
-        assert_eq!(other, expected);
+        let other = number.set_bit_unchecked(2);
+        assert_eq!(other, 0b00110);
+    }
+
+    #[test]
+    fn set_bit() {
+        let number: i8 = 0b00010;
+        let other: Option<i8> = number.set_bit(2);
+        assert_eq!(other, Some(0b00110));
+    }
+
+    #[test]
+    fn set_range() {
+        let number: i32 = 0b100;
+        let other: i32 = number.set_range(..10);
+        assert_eq!(other, 0b1111111111);
+        let other: i32 = number.set_range(0..10);
+        assert_eq!(other, 0b1111111111);
+        let other: i32 = number.set_range(5..7);
+        assert_eq!(other, 0b01100100);
+        let other: i32 = number.set_range(5..=7);
+        assert_eq!(other, 0b11100100);
+    }
+
+    #[allow(overflowing_literals)]
+    #[test]
+    fn set() {
+        let other: i8 = 0b100.set_all();
+        assert_eq!(other, 0b11111111);
+
+        let other: i16 = 0b100.set_all();
+        assert_eq!(other, 0b1111111111111111);
+
+        let other: i32 = 0b100.set_all();
+        assert_eq!(other, 0b11111111111111111111111111111111);
+
+        let other: i64 = 0b100.set_all();
+        assert_eq!(
+            other,
+            0b1111111111111111111111111111111111111111111111111111111111111111
+        );
+
+        let other: i128 = 0b100.set_all();
+        assert_eq!(
+            other,
+            0b11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
+        );
+    }
+
+    #[test]
+    fn update_bit_unchecked() {
+        let other: i8 = 0b00010.update_bit_unchecked(4, true);
+        assert_eq!(other, 0b10010);
+    }
+
+    #[test]
+    fn update_bit() {
+        let other: Option<i8> = 0b00010.update_bit(4, true);
+        assert_eq!(other, Some(0b10010));
+    }
+
+    #[test]
+    fn clear_bit_unchecked() {
+        let other: i8 = 0b00010001.clear_bit_unchecked(4);
+        assert_eq!(other, 0b00000001);
+    }
+
+    #[test]
+    fn clear_bit() {
+        let other: Option<i8> = 0b00010001.clear_bit(4);
+        assert_eq!(other, Some(0b00000001));
+    }
+
+    #[test]
+    fn clear() {
+        let other: i8 = 0b00010001.clear();
+        assert_eq!(other, 0b00000000);
+    }
+
+    #[test]
+    fn flip_bit_unchecked() {
+        let other: i8 = 0b00010001.flip_bit_unchecked(2);
+        assert_eq!(other, 0b00010101);
+    }
+
+    #[test]
+    fn flip_bit() {
+        let other: Option<i8> = 0b00010001.flip_bit(2);
+        assert_eq!(other, Some(0b00010101));
+    }
+
+    #[allow(overflowing_literals)]
+    #[test]
+    fn flip() {
+        let other: i8 = 0b00010001.flip();
+        assert_eq!(other, 0b11101110);
+    }
+
+    #[test]
+    fn parity() {
+        let other: bool = 0b00000001.parity();
+        assert_eq!(other, true);
+
+        let other: bool = 0b10100000.parity();
+        assert_eq!(other, false);
+    }
+
+    #[test]
+    fn hamming_distance() {
+        let other: usize = 0b00000000.hamming_distance(0b11000001);
+        assert_eq!(other, 3);
+    }
+
+    #[test]
+    fn zero() {
+        assert_eq!(i8::zero(), 0i8);
+    }
+
+    #[test]
+    fn one() {
+        assert_eq!(i8::one(), 1i8);
+    }
+
+    #[test]
+    fn bit_size() {
+        assert_eq!(i8::bit_size(), 8usize);
     }
 }
